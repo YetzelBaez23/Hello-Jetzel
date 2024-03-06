16033a452ad2172eae9bceccb3d2eb4b5553e6ee
YetzelBaez23 <160265351+YetzelBaez23@users.noreply.github.com>
2024-03-05 18:53:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-
-    let name = prompt("¿Cual es tu nombre?");
-    if (name != null) {
-    document.getElementById("demo").innerHTML= "hola " + name + " Bienvenido!";
+function enviarSaludo(){
+    let nombre = prompt("¿como te llamas?");
+    
+    if (nombre != null){
+     document.getElementById("demo").innerHTML = `Hey there ${nombre}!`
+    } 
+    else{
+        document.getElementById("demo").innerHTML = `Nimodo la vida sigue...`
     }
-
+}
